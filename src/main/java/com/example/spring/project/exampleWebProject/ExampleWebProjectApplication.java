ab31b796bba7e8b0e21f5d4d3ca893deb001edfd
Murat Ozturk <murat.ozturk@sahibinden.com>
2019-01-02 22:27:21 +0300

    first commit

---
+++ src/main/java/com/example/spring/project/exampleWebProject/ExampleWebProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring.project.exampleWebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleWebProjectApplication.class, args);
+	}
+
+}
+
